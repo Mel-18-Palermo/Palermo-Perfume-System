f9e5a630cbbdd7fbfdbe6e28cc0c904638079355
HexCodeYT <indices-chants-7u@icloud.com>
2026-09-08 14:07:14 +1000

    test(checkout): cover idempotency and stock conflicts

--- tests/integration/database.test.ts
+++ tests/integration/database.test.ts
@@ -10,6 +10,7 @@ import { cartCases } from "./cart-cases";
 import { profileCases } from "./profile-cases";
 import { wishlistCases } from "./wishlist-cases";
 import { discoveryCases } from "./discovery-cases";
+import { checkoutCases } from "./checkout-cases";
 
 const testUrl = process.env["TEST_DATABASE_URL"];
 assertDevelopmentDatabase(testUrl, true);
@@ -22,6 +23,7 @@ cartCases(db);
 profileCases(db);
 wishlistCases(db);
 discoveryCases(db);
+checkoutCases(db);
 
 beforeAll(async () => {
   await pool.query("SET search_path = palermo_test");
